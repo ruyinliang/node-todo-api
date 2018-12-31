0e115757f7aa76e86f0a91251fa3b040578b3115
ruyin <zalry995@gmail.com>
2018-12-31 20:33:08 +0800

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -18,9 +18,11 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
-})
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
